23d06a739303dcbf833c01a73b2ad4b6c3db3e50
sorpa'as plat <sorpaas@gmail.com>
2014-12-07 20:34:40 +0800

    Add back old emacs configurations

--- custom.el
+++ custom.el
@@ -5,16 +5,16 @@
 
 ;; For example, oh-my-emacs disables menu-bar-mode by default. If you want it
 ;; back, just put following code here.
-(menu-bar-mode t)
+;; (menu-bar-mode t)
 
 ;;; You email address
-(setq user-mail-address "xiaohanyu1988@gmail.com")
+(setq user-mail-address "sorpaas@gmail.com")
 
 ;;; Calendar settings
 ;; you can use M-x sunrise-sunset to get the sun time
-(setq calendar-latitude 39.9)
-(setq calendar-longitude 116.3)
-(setq calendar-location-name "Beijing, China")
+(setq calendar-latitude 22.2670)
+(setq calendar-longitude 114.1880)
+(setq calendar-location-name "Hong Kong")
 
 ;;; Time related settings
 ;; show time in 24hours format
@@ -62,3 +62,21 @@ inversion of gas-comment-region"
           (delete-char 1))
       (next-line))
     (goto-char end)))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
+ '(haskell-notify-p t)
+ '(haskell-stylish-on-save t)
+ '(haskell-tags-on-save t)
+ '(safe-local-variable-values (quote ((no-byte-compile t)))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
